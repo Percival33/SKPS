17c5309de90b713374d6fa8bed6fddaf03cbac53
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-06-15 01:00:00 +0200

    fix socket addresses

--- projekt/client.py
+++ projekt/client.py
@@ -24,6 +24,7 @@ def generate_angles():
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.connect((HOST, PORT))
 
     angle_generator = generate_angles()
